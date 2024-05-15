6a1d3ed4d96a46a89a9fa11fcb6d9df876978d10
BrenoSantoS2 <breno.santos.ismart@gmail.com>
2024-05-15 18:54:17 -0300

    Version 0.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../css/fonts.css";
+import "../css/style.css";
 import { HeroSection } from "../components/heroSection";
+import { AboutSection } from "../components/aboutSection";
 
 export default function Page() {
   return (
     <div>
         <HeroSection />
+        <AboutSection />
         <h1>Page</h1>
     </div>
   );
---
+++ src/components/aboutSection.tsx
@@ -0,0 +1,21 @@
+import Styles from "../css/css_components/aboutSection.module.css";
+import "../css/style.css"
+
+export function AboutSection() {
+    return (
+        <section>
+            <div>
+                <div className={Styles.about_section}>
+                    <div className= {Styles.content}>
+                        <div className={Styles.title_layout}>
+                            <h3>About Us</h3>
+                            <h1>Creating Games That Inspire and Entertain</h1>
+                        </div>
+
+                        <p className={Styles.text_position}>Calcifer Studios is a game studio founded by university students with a mission to create captivating and innovative games that bring joy to players around the world. We believe in the power of games to inspire and entertain, and we are dedicated to delivering unique and immersive experiences.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
